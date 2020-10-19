2938a4d880870742fc1a1f7d2a6279ebb48ac3d4
Siddharatha.H.Dhumale <shdhumale@gmail.com>
2020-10-19 19:28:13 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+//import web-push
+const webpush = require("web-push");
+//import body-parser
+const bodyParser = require("body-parser");
+//import path
+const path = require("path");
+const app = express();
+
+// This serves static files from the specified directory
+app.use(express.static(path.join(__dirname + '/build')));
+app.use(bodyParser.json());
+
+//You can create below given client public and privet key using following command
+// .\node_modules\.bin\web-push generate-vapid-keys
+const publicVapidKey = "BBw9IKZvRihyuFCyS5Mqz6vn1cGXf-b9etPnKZt-22vK87-rGuzYihqCjbpB9PqkgD8CvEyb3xZi6JEQU-x5-wk";
+const privateVapidKey = "ZwD1zwXE38TBT7KKlWMzWTJb-Y3__f--U7CBPFinKnY";
+
+
+//Setting the details that will be required by the webpush api. You can get this code from https://github.com/web-push-libs/web-push
+webpush.setVapidDetails(
+  "mailto:test@test.com",
+  publicVapidKey,
+  privateVapidKey
+);
+
+// Subscribe Route :- This will be used to send the subscribe request from the client to service worker
+app.post("/subscribe", (req, res) => {
+  // Get pushSubscription object
+  const subscription = req.body;
+  console.log("subscription:::", subscription);
+
+  // Send 201 - resource created :- Then we send back the status 201 that indicate the resource is created successfully in the nodejs server for the subscription request made by the client.
+  res.status(201).json({});
+
+  // Create payload:- that we used to send it to the client that will be shown to the end user.
+  const payload = JSON.stringify({ title: "Push Test" });
+
+  // Pass object into sendNotification :- In this step we will send this subscription and payload to the client using web.pushSendNotification
+  webpush
+    .sendNotification(subscription, payload)
+    .catch(err => console.error(err));
+});
+
+
+const server = app.listen(8081, () => {
+
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
---
+++ src/js/ui.js
@@ -0,0 +1,36 @@
+const users = document.querySelector('.users');
+
+document.addEventListener('DOMContentLoaded', function () {
+  // nav menu to show menu.
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, { edge: 'right' });
+  // add user form
+  const forms = document.querySelectorAll('.side-form');
+  M.Sidenav.init(forms, { edge: 'left' });
+});
+
+
+// render User data
+const userAdded = (data, id) => {
+  const html = `<div class="card-panel user white row" data-id="${id}">
+            <img src="/img/siddhu.jpg" alt="user thumb">
+            <div class="user-details">
+                <div class="user-title">${data.userName}</div>
+                <div class="user-address">${data.userAddress}</div>
+            </div>
+            <div class="user-delete">
+                <i class="material-icons" data-id="${id}">delete_outline</i>
+            </div>
+        </div>
+        
+  `;
+  users.innerHTML += html;
+
+};
+// remove user
+const userRemoved
+  = (id) => {
+    const user = document.querySelector(`.user[data-id=${id}]`);
+    user.remove();
+  };
+
